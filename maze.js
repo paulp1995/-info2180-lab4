6e0f95a5b38a134b59c96ae5a1c026defb0adf26
paulp1995 <paul.parker1995@gmail.com>
2017-10-21 16:00:55 -0400

    Ex 1
    Signed-off-by: paulp1995 <paul.parker1995@gmail.com>

---
+++ maze.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+
+var book = document.getElementById("boundary1");
+
+book.addEventListener("mouseover", function() {
+
+
+	book.setAttribute("class","boundary youlose");
+});
+
+
+
+
+
+}
