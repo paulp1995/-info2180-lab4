98d373e24b122cd992bc0d9c773eed23251d0228
paulp1995 <paul.parker1995@gmail.com>
2017-10-21 21:36:19 -0400

    Ex 5 Signed-off-by: paulp1995 <paul.parker1995@gmail.com>

--- maze.js
+++ maze.js
@@ -1,63 +1,52 @@
-var chec =  false;
+var chec = false; //keep track if the user hit any maze walls.
+  
+  
+   window.onload = function() {
 
 
-window.onload = function(){
+      var un = document.getElementById("end");
 
-
-	var un = document.getElementById("end");
-	un.addEventListener("mouseover",gameEnd);
-    
-
-    var re = document.getElementById("start");
-    re.addEventListener("mouseover",startGme);
-
-
-var boun = document.querySelectorAll("div#maze div.boundary");
-
-
-	for (var p=0; p<boun.length; p++)
-	{
-		boun[p].addEventListener("mouseover",redTurn);
-	}
+       un.addEventListener("mouseover",gameEnd);
 
 
 
+        var re = document.getElementById("start");
 
-}
-
-
-function redTurn (){
-	chec = true;
-		var boun = document.querySelectionorAll("div#maze div.boundary");
-		for (var p =0;p< boun.length ; p++) {
-			boun[p].setAttribute("class","boundary youlose");
-		}
-}
-
-function gameEnd(){
-    var ss=document.getElementById("status");
-
-	if (chec){
-		ss.innerHTML="game over! You lost.";
-	}
-	else {
-		ss.innerHTML = "great job! You won ";
-	}
-}
+     re.addEventListener("click",startGame);
 
 
-function startGame(){
-
-	chec= false;
-	var boun=document.querySelectorAll"div#maze div.boundary");
-	for (var p =0;p< boun.length ; p++) {
-			boun[p].setAttribute("class","boundary");
-		}
+       var boun = document.querySelectorAll("div#maze div.boundary");
+ 
+  
+  
+  function redTurn (){
+ 
+   
+    chec = true;
+      var boun = document.querySelectionorAll("div#maze div.boundary");
+      for (var p =0;p< boun.length ; p++) {
+      allboun[p].setAttribute("class","boundary youlose");
+       boun[p].setAttribute("class","boundary youlose");
+      }
+  }
 
 
 
+   function gameEnd(){
+       var ss =document.getElementById("status")
 
-
-
-}
-}
+       if(chec) {
+       ss.innerHTML="You Lost.";
+      } else {
+       ss.innerHTML="great job! You won ";
+       
+       }
+  }
+  
+  function startGame(){
+ 
+   chec= false;
+   var boun=document.querySelectorAll"div#maze div.boundary");
+   for (var p =0;p< boun.length ; p++) {
+       boun[p].setAttribute("class","boundary");
+     }
