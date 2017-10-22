816cce0f793aa2d01cf04b2e75e2c497effbf5e6
paulp1995 <paul.parker1995@gmail.com>
2017-10-21 20:02:01 -0400

    ex3 Signed-off-by: paulp1995 <paul.parker1995@gmail.com>

--- maze.js
+++ maze.js
@@ -1,8 +1,17 @@
+var chec =  false;
+
+
 window.onload = function(){
 
 
+	var un = document.getElementById("end");
+	un.addEventListener("mouseover",gameEnd);
 
-var boun = document.querySelectionorAll("div#maze div.boundary");
+
+
+var boun = document.querySelectorAll("div#maze div.boundary");
+
+
 	for (var p=0; p<boun.length; p++)
 	{
 		boun[p].addEventListener("mouseover",redTurn);
@@ -12,9 +21,21 @@ var boun = document.querySelectionorAll("div#maze div.boundary");
 
 
 }
-function redTurn(){
-	var boun= document.querySelectionorAll("div#maze div.boundary");
-	for (var p =0;p< boun.length ; p++) {
-		allboun[p].setAttribute("class","boundary youlose");
+
+
+function redTurn (){
+	chec = true;
+		var boun = document.querySelectionorAll("div#maze div.boundary");
+		for (var p =0;p< boun.length ; p++) {
+			allboun[p].setAttribute("class","boundary youlose");
+		}
+}
+
+function gameEnd(){
+	if (chec){
+		alert("game over! You lost.");
+	}
+	else {
+		alert("great job! You won ");
 	}
 }
