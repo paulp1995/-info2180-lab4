8578f3841ceff471164d3f84dc7395e5f0c456c6
paulp1995 <paul.parker1995@gmail.com>
2017-10-21 22:43:00 -0400

    ex 6
    Signed-off-by: paulp1995 <paul.parker1995@gmail.com>

--- maze.js
+++ maze.js
@@ -1,52 +1,55 @@
 var chec = false; //keep track if the user hit any maze walls.
   
-  
-   window.onload = function() {
+  window.onload = function() {
+
+      var un  =   document.getElementById("end");
+
+      un.addEventListener("mouseover",gameEnd);
 
 
-      var un = document.getElementById("end");
-
-       un.addEventListener("mouseover",gameEnd);
-
-
-
-        var re = document.getElementById("start");
+     var re = document.getElementById("start");
 
      re.addEventListener("click",startGame);
 
-
+      
        var boun = document.querySelectorAll("div#maze div.boundary");
- 
-  
-  
-  function redTurn (){
- 
-   
-    chec = true;
-      var boun = document.querySelectionorAll("div#maze div.boundary");
-      for (var p =0;p< boun.length ; p++) {
-      allboun[p].setAttribute("class","boundary youlose");
-       boun[p].setAttribute("class","boundary youlose");
-      }
-  }
 
 
-
-   function gameEnd(){
-       var ss =document.getElementById("status")
-
-       if(chec) {
-       ss.innerHTML="You Lost.";
-      } else {
-       ss.innerHTML="great job! You won ";
-       
-       }
-  }
-  
-  function startGame(){
- 
-   chec= false;
-   var boun=document.querySelectorAll"div#maze div.boundary");
-   for (var p =0;p< boun.length ; p++) {
-       boun[p].setAttribute("class","boundary");
+    for (var p= 0; p< boun.length; p++){
+    boun[p].addEventListener("mouseover", redTurn);
      }
+ };
+ 
+ function redTurn() {
+  chec = true;
+     var boun = document.querySelectorAll("div#maze div.boundary");
+     for (var p = 0; p < boun.length; p++){
+         boun[p].setAttribute("class", "boundary youlose");
+     }
+ }
+ 
+ function gameEnd() {
+   var ss= document.getElementById("status");
+     if(chec) {
+         ss.innerHTML="You Lost.";
+      } else {
+         ss.innerHTML="great job! You won ";
+      }
+ }
+
+
+
+ 
+ function startGame() {
+  chec = false;
+     var boun = document.querySelectorAll("div#maze div.boundary");
+   document.addEventListener("mouseover", function(){
+      if (event.clientX < maze.offsetLeft || event.clientY > maze.offsetRight)
+      {
+        redTurn();
+      }
+     });
+     for (var p= 0; p< boun.length; p++){
+         boun[p].setAttribute("class", "boundary");
+     }
+  } 
